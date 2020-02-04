15374348c68644a49345848fce70e0f95d3bcce1
Clemens Hemmerling <clemenshemmerling@gmail.com>
2020-02-03 21:47:12 -0600

    first commit

---
+++ src/components/Badge.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './styles/Badge.css';
+import logo from '../images/badge-header.svg';
+import avatar from '../images/avatar.png';
+
+
+
+class Badge extends React.Component {
+  render() {
+    return (
+      <div className="Badge">
+        <div className="Badge__header">
+          <img src={logo} alt="Logo" />
+        </div>
+
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar" 
+            src={avatar} 
+            alt="Avatar"
+          />
+          <h1>Clemens <br/>Hemmerling</h1>
+        </div>
+        
+        <div className="Badge__section-info">
+          <p>Frontend</p>
+          <p>@clemens</p>
+        </div>
+
+        <div className="Badge__footer">#Clemens</div>
+
+      </div>
+    );
+  }
+}
+
+export default Badge;
